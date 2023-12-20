ae7d665f2b19ad52bd1acdc105dc5d7a722ef4cd
dclxxi <dclxxi@kakao.com>
2023-12-20 14:27:25 +0900

    feat: 웹캠 연결 기능 추가

--- monitoring/live/models.py
+++ monitoring/live/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CameraImage(models.Model):
+    image = models.ImageField(upload_to="이미지가 저장되는 디렉토리")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Lecture(models.Model):
+    topic = models.CharField(max_length=50, null=False)
+    datetime = models.DateTimeField(auto_now=True)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+    class Meta:
+        db_table = 'lecture'
+
+
+class Feedback(models.Model):
+    time = models.TimeField()
+    content = models.TextField()
+
+    class Meta:
+        db_table = 'feedback'
--- monitoring/live/views.py
+++ monitoring/live/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from live.models import CameraImage
+
+
 # Create your views here.
+def info(request):
+    return HttpResponse("live/info 응답")
+
+
+def cam(request):
+    if request.method == 'POST':
+        image = request.FILES.get('camera-image')
+        CameraImage.objects.create(image=image)
+    images = CameraImage.objects.all()
+    context = {
+        'images': images
+    }
+    return render(request, 'cam.html', context)
