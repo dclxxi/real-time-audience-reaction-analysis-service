dea44b04ed644da250e53276c2c9cec6336c5591
zoooomin <angel458676@gmail.com>
2023-12-20 16:55:15 +0900

    style: live.models->.models

--- monitoring/live/views.py
+++ monitoring/live/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
-from live.models import CameraImage
+from .models import CameraImage
 
 
 # Create your views here.
