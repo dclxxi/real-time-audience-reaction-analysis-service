ef410ea96c997d14af2dd88e9a12def1ad3ee624
dclxxi <dclxxi@kakao.com>
2023-12-21 17:41:04 +0900

    feat: 회원 가입 기능 추가

--- monitoring/user/views.py
+++ monitoring/user/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth.hashers import make_password, check_password
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from user.models import User
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        userid = request.POST.get('userid')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+
+        if User.objects.filter(userid=userid).exists():
+            return render(request, 'user/signup.html')
+
+        if password != password_confirm:
+            return render(request, 'user/signup.html')
+
+        user = User()
+        user.userid = userid
+        user.password = make_password(password)  # 비밀번호 암호화
+        user.name = name
+        user.email = email
+        user.save()
+
+        return HttpResponse("signup successful")
+
+    return render(request, 'user/signup.html')
+
