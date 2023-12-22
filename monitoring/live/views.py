b7cf5a0c8207650822518058d0a6e495d0ee1cd7
dclxxi <dclxxi@kakao.com>
2023-12-22 16:32:00 +0900

    feat: 이미지 blob에서 jpg로 저장되도록 수정

--- monitoring/live/views.py
+++ monitoring/live/views.py
@@ -1,6 +1,7 @@
 import os
 from uuid import uuid4
 
+from PIL import Image
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
@@ -54,16 +55,16 @@ def record(request, id, term):
 def get_capture_file(request):
     if request.method == "POST" and 'file' in request.FILES:
         file = request.FILES['file']
-        print(file.name)
+        img = Image.open(file)
 
-        uuid_name = uuid4().hex  # 이미지 이름이 한글, 공백이 섞여있기 때문에 uuid를 이용해서 이름을 바꿈
+        uuid_name = uuid4().hex
         save_path = os.path.join(MEDIA_ROOT, uuid_name)
 
-        with open(save_path, "wb+") as destination:
-            for chunk in file.chunks():
-                destination.write(chunk)
+        img = img.convert('RGB')
+        jpg_save_path = save_path + '.jpg'
+        img.save(jpg_save_path, 'JPEG')
 
-    return HttpResponse(file)
+        return HttpResponse(jpg_save_path)
 
 
 def lecture_list(request):
