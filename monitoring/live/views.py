f5e109b226469ff54eb590bb318e5952284a8c50
zoooomin <angel458676@gmail.com>
2024-01-03 14:56:41 +0900

    feat: 강의 주제 전달

--- monitoring/live/views.py
+++ monitoring/live/views.py
@@ -29,7 +29,9 @@ credentials = service_account.Credentials.from_service_account_file(KEY_PATH)
 
 client = storage.Client(credentials=credentials, project=credentials.project_id)
 
-openai = OpenAI(api_key="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")  # 본인 API키로 수정해서 실행해주세요
+openai = OpenAI(
+    api_key="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"
+)  # 본인 API키로 수정해서 실행해주세요
 
 
 # Create your views here.
@@ -48,7 +50,7 @@ def info(request):
         lecture.user = get_object_or_404(User, pk=request.user.pk)
         lecture.save()
 
-        request.session['lecture_id'] = lecture.id
+        request.session["lecture_id"] = lecture.id
 
         return redirect("live:record", id=lecture.id, term=term)
 
@@ -56,13 +58,19 @@ def info(request):
 @csrf_exempt
 @login_required
 def record(request, id, term):
-    session_lecture_id = request.session.get('lecture_id')
+    session_lecture_id = request.session.get("lecture_id")
 
     if not session_lecture_id or session_lecture_id != id:
         return redirect("live:info")
 
     if request.method == "GET":
-        return render(request, "live/analysis_page.html", context=dict(id=id, term=term))
+        lecture = get_object_or_404(Lecture, id=id)
+
+        return render(
+            request,
+            "live/analysis_page.html",
+            context=dict(id=id, term=term, topic=lecture.topic),
+        )
 
     if request.method == "POST":
         pass
@@ -71,8 +79,8 @@ def record(request, id, term):
 @csrf_exempt
 def get_capture_file(request):
     if request.method == "POST" and "image" in request.FILES:
-        lecture_id = request.POST.get('lecture_id')
-        time = request.POST.get('time')
+        lecture_id = request.POST.get("lecture_id")
+        time = request.POST.get("time")
         image = request.FILES["image"]
 
         uuid_name = uuid4().hex
@@ -102,9 +110,9 @@ def get_capture_file(request):
 @csrf_exempt
 def time(request):
     if request.method == "POST":
-        lecture_id = request.POST.get('lecture_id')
-        start_time = request.POST.get('start_time')
-        end_time = request.POST.get('end_time')
+        lecture_id = request.POST.get("lecture_id")
+        start_time = request.POST.get("start_time")
+        end_time = request.POST.get("end_time")
 
         lecture = get_object_or_404(Lecture, pk=lecture_id)
         lecture.start_time = start_time
@@ -116,9 +124,13 @@ def time(request):
 
 @csrf_exempt
 def get_video_file(request):
-    if request.method == "POST" and "video" in request.FILES and "image" in request.FILES:
-        lecture_id = request.POST.get('lecture_id')
-        time = request.POST.get('time')
+    if (
+        request.method == "POST"
+        and "video" in request.FILES
+        and "image" in request.FILES
+    ):
+        lecture_id = request.POST.get("lecture_id")
+        time = request.POST.get("time")
         image = request.FILES["image"]
         video = request.FILES["video"]
 
@@ -140,10 +152,10 @@ def get_video_file(request):
         reaction = Reaction()
         reaction.lecture = lecture
         reaction.time = time
-        reaction.concentration = results.get('concentration')
-        reaction.negative = results.get('negative')
-        reaction.neutral = results.get('neutral')
-        reaction.positive = results.get('positive')
+        reaction.concentration = results.get("concentration")
+        reaction.negative = results.get("negative")
+        reaction.neutral = results.get("neutral")
+        reaction.positive = results.get("positive")
         reaction.save()
 
         blob_name = f"{uuid_name}.mp4"
@@ -165,7 +177,7 @@ def get_video_file(request):
         print(mp3_blob.public_url)
 
         audio_content = run_stt(upload_file_name)
-        print('출력: ', audio_content)
+        print("출력: ", audio_content)
 
         # feedback_content = chatGPT(lecture.topic, audio_content, reaction)
         feedback_content = f"{lecture.topic} 주제의 피드백입니다."
@@ -195,13 +207,30 @@ def model(blob_path):
         x_off, y_off = offsets
         return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
 
-    def draw_text(coordinates, image_array, text, color, x_offset=0, y_offset=0, font_scale=0.5, thickness=2):
+    def draw_text(
+        coordinates,
+        image_array,
+        text,
+        color,
+        x_offset=0,
+        y_offset=0,
+        font_scale=0.5,
+        thickness=2,
+    ):
         x, y = coordinates[:2]
-        cv2.putText(image_array, text, (x + x_offset, y + y_offset), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color,
-                    thickness, cv2.LINE_AA)
+        cv2.putText(
+            image_array,
+            text,
+            (x + x_offset, y + y_offset),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            font_scale,
+            color,
+            thickness,
+            cv2.LINE_AA,
+        )
 
     def preprocess_input(x, v2=True):
-        x = x.astype('float32')
+        x = x.astype("float32")
         x = x / 255.0
         if v2:
             x = x - 0.5
@@ -209,9 +238,17 @@ def model(blob_path):
         return x
 
     image_path = blob_path
-    detection_model_path = 'haarcascade_frontalface_default.xml'
-    emotion_model_path = 'emotion_temp.h5'
-    emotion_labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'sad', 5: 'surprise', 6: 'neutral'}
+    detection_model_path = "haarcascade_frontalface_default.xml"
+    emotion_model_path = "emotion_temp.h5"
+    emotion_labels = {
+        0: "angry",
+        1: "disgust",
+        2: "fear",
+        3: "happy",
+        4: "sad",
+        5: "surprise",
+        6: "neutral",
+    }
     font = cv2.FONT_HERSHEY_SIMPLEX
     emotion_offsets = (0, 0)
     face_detection = load_detection_model(detection_model_path)
@@ -222,17 +259,31 @@ def model(blob_path):
 
     def highest_emotion(positive, neutral, negative):
         if positive == max([positive, neutral, negative]):
-            return 'positive'
+            return "positive"
         elif neutral == max([positive, neutral, negative]):
-            return 'neutral'
+            return "neutral"
         else:
-            return 'negative'
+            return "negative"
 
     def engagement_score(scores):
-        if ((scores[6] > 0.6) | (scores[3] > 0.5) | (scores[5] > 0.6) | (scores[0] > 0.2) | (scores[1] > 0.2) | (
-                scores[2] > 0.3) | (scores[4] > 0.3)):
-            return ((scores[0] * 0.2) + (scores[1] * 0.2) + (scores[2] * 0.3) + (scores[3] * 0.7) + (
-                    scores[4] * 0.3) + (scores[5] * 0.7) + (scores[6] * 1.0))
+        if (
+            (scores[6] > 0.6)
+            | (scores[3] > 0.5)
+            | (scores[5] > 0.6)
+            | (scores[0] > 0.2)
+            | (scores[1] > 0.2)
+            | (scores[2] > 0.3)
+            | (scores[4] > 0.3)
+        ):
+            return (
+                (scores[0] * 0.2)
+                + (scores[1] * 0.2)
+                + (scores[2] * 0.3)
+                + (scores[3] * 0.7)
+                + (scores[4] * 0.3)
+                + (scores[5] * 0.7)
+                + (scores[6] * 1.0)
+            )
         else:
             return 0
 
@@ -252,10 +303,17 @@ def model(blob_path):
 
         emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
         engagement = engagement_score(emotion_classifier.predict(gray_face)[0])
-        positive = emotion_classifier.predict(gray_face)[0][3] + emotion_classifier.predict(gray_face)[0][5]
+        positive = (
+            emotion_classifier.predict(gray_face)[0][3]
+            + emotion_classifier.predict(gray_face)[0][5]
+        )
         neutral = emotion_classifier.predict(gray_face)[0][6]
-        negative = emotion_classifier.predict(gray_face)[0][0] + emotion_classifier.predict(gray_face)[0][1] + \
-                   emotion_classifier.predict(gray_face)[0][2] + emotion_classifier.predict(gray_face)[0][4]
+        negative = (
+            emotion_classifier.predict(gray_face)[0][0]
+            + emotion_classifier.predict(gray_face)[0][1]
+            + emotion_classifier.predict(gray_face)[0][2]
+            + emotion_classifier.predict(gray_face)[0][4]
+        )
         positives.append(positive)
         neutrals.append(neutral)
         negatives.append(negative)
@@ -266,7 +324,7 @@ def model(blob_path):
         draw_bounding_box(face_coordinates, rgb_image, color)
         draw_text(face_coordinates, rgb_image, emotion_text, color, -20, -20, 0.7, 2)
     plt.imshow(rgb_image)
-    cv2.imwrite('result_emotion_image.jpg', rgb_image)
+    cv2.imwrite("result_emotion_image.jpg", rgb_image)
 
     def calculate_percentage(emotion):
         if len(emotion) == 0:
@@ -313,7 +371,7 @@ def run_stt(upload_file_name):
     res = client.long_running_recognize(config=config, audio=audio)
     response = res.result()
 
-    audio_content = ''
+    audio_content = ""
     for result in response.results:
         audio_content += result.alternatives[0].transcript
 
@@ -321,22 +379,29 @@ def run_stt(upload_file_name):
 
 
 def chatGPT(topic, prompt, reaction):
-    content = (f"강의 주제: {topic}\n"
-               f"청중 반응: 집중도 {reaction.concentration}%, 긍정 {reaction.positive}%, "
-               f"중립 {reaction.neutral}%, 부정 {reaction.negative}%\n"
-               f"강의 내용: {prompt}")
-    completion = openai.chat.completions.create(model="gpt-3.5-turbo",
-                                                messages=[
-                                                    {"role": "system", "content": "당신은 교육 전문가로서, 강의 내용에 대한 피드백을 "
-                                                                                  "제공해야 합니다. 강점, 약점, 개선 방법을 순서대로 작성해주세요."
-                                                                                  "발음이나 억양, 속도 등의 음성적 특성에 대한 피드백은 제외하고, "
-                                                                                  "오로지 강의 내용과 청중의 반응에 초점을 맞춰주세요.\n"
-                                                                                  "[피드백 형식]\n"
-                                                                                  "강점: [한 줄로 강점을 기재해 주세요]\n"
-                                                                                  "약점: [한 줄로 약점을 기재해 주세요]\n"
-                                                                                  "개선 방법: [한 줄로 개선 방법을 기재해 주세요]"},
-                                                    {"role": "user", "content": content}
-                                                ])
+    content = (
+        f"강의 주제: {topic}\n"
+        f"청중 반응: 집중도 {reaction.concentration}%, 긍정 {reaction.positive}%, "
+        f"중립 {reaction.neutral}%, 부정 {reaction.negative}%\n"
+        f"강의 내용: {prompt}"
+    )
+    completion = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "당신은 교육 전문가로서, 강의 내용에 대한 피드백을 "
+                "제공해야 합니다. 강점, 약점, 개선 방법을 순서대로 작성해주세요."
+                "발음이나 억양, 속도 등의 음성적 특성에 대한 피드백은 제외하고, "
+                "오로지 강의 내용과 청중의 반응에 초점을 맞춰주세요.\n"
+                "[피드백 형식]\n"
+                "강점: [한 줄로 강점을 기재해 주세요]\n"
+                "약점: [한 줄로 약점을 기재해 주세요]\n"
+                "개선 방법: [한 줄로 개선 방법을 기재해 주세요]",
+            },
+            {"role": "user", "content": content},
+        ],
+    )
     print(completion)
     result = completion.choices[0].message.content
 
@@ -344,9 +409,7 @@ def chatGPT(topic, prompt, reaction):
 
 
 def imageGPT(prompt):
-    response = openai.images.generate(prompt=prompt,
-                                      n=1,
-                                      size="256x256")
-    result = response['data'][0]['url']
+    response = openai.images.generate(prompt=prompt, n=1, size="256x256")
+    result = response["data"][0]["url"]
 
     return result
