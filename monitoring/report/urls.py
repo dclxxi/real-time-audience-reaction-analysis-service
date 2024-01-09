c7a000a9933cd9fa50949382f408a79d334a9074
dclxxi <dclxxi@kakao.com>
2024-01-09 13:39:53 +0900

    test: report 테스트 코드 추가

--- monitoring/report/urls.py
+++ monitoring/report/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'report'
 
 urlpatterns = [
-    path("list/", views.list_lectures),
+    path("list/", views.list_lectures, name="list_lectures"),
     path("result/<int:id>/", views.result, name="detail"),
-    path("list/delete/", views.delete_lecture),
+    path("list/delete/", views.delete_lecture, name="delete_lecture"),
 ]
