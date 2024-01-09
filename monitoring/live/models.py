0ce2b7dc7437b05176c7e0437b994976e3c1e154
dclxxi <dclxxi@kakao.com>
2024-01-09 13:47:52 +0900

    feat: lecture 테이블에 category 추가

--- monitoring/live/models.py
+++ monitoring/live/models.py
@@ -8,6 +8,7 @@ from user.models import User
 class Lecture(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     topic = models.TextField(max_length=50, null=False)
+    category = models.TextField(max_length=50, null=False)
     datetime = models.DateTimeField(auto_now=True)
     start_time = models.TimeField(null=True)
     end_time = models.TimeField(null=True)
